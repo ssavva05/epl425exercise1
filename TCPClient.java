f732db9753c25777d5f0cd479f9668ed036d1b6b
abecket <agathe.duboue@insa-rennes.fr>
2017-10-06 18:01:41 +0300

    Changes

--- TCPClient.java
+++ TCPClient.java
@@ -14,50 +14,67 @@ public class TCPClient implements Runnable {
 
 	
 	private static final int SIZE_OF_POOL = 10;
-	private static final int NUMBER_OF_CLIENTS = 100;
+	private static final int NUMBER_OF_CLIENTS = 15;
 	private static final int NUMBER_OF_REQUESTS_PER_CLIENTS = 5;
     	private static String IP;
     	private static String Port;
-    	private int counter;
+    	private int userid;
+    	private long totalTimeSpentPerClient;
+    	private long averageTimeSpentPerClient;
 
-        public TCPClient(int count) {
-            this.counter = count;
+        public TCPClient(int userid) {
+            this.userid = userid;
+            this.totalTimeSpentPerClient = 0;
+            this.averageTimeSpentPerClient = 0;
         }
 
         @Override
         public void run() {
-		for (int i=0;i<NUMBER_OF_REQUESTS_PER_CLIENTS;i++) {
-		try { 		
-		Socket socket = new Socket(IP, Integer.parseInt(Port));
+        	for (int i=1;i<=NUMBER_OF_REQUESTS_PER_CLIENTS;i++) {
+        			try { 		
+        				Socket socket = new Socket(IP, Integer.parseInt(Port));
 		          
-                String message, response;
+        				String message, response;
 
-                DataOutputStream output = new DataOutputStream(socket.getOutputStream());
-                BufferedReader server = new BufferedReader(
-                        new InputStreamReader(socket.getInputStream())
-                );
+        				DataOutputStream output = new DataOutputStream(socket.getOutputStream());
+        				BufferedReader server = new BufferedReader(new InputStreamReader(socket.getInputStream()));
 		 
-                String clientMessage = "HELLO "+ Inet4Address.getLocalHost().getHostAddress() + " " +  socket.getLocalPort() + " " +  this.counter + System.lineSeparator();
-                Reader clientMessageReader = new StringReader(clientMessage);
-                BufferedReader messageFromClient = new BufferedReader(clientMessageReader);                
+        				String clientMessage = "HELLO "+ Inet4Address.getLocalHost().getHostAddress() + " " +  socket.getLocalPort() + " " +  this.userid + System.lineSeparator();
+        				Reader clientMessageReader = new StringReader(clientMessage);
+        				BufferedReader messageFromClient = new BufferedReader(clientMessageReader);                
 
-                message = messageFromClient.readLine() + System.lineSeparator();
+        				message = messageFromClient.readLine() + System.lineSeparator();
 
-                output.writeBytes(message);
-                response = server.readLine();
-
-                System.out.println("[" + new Date() + "] Received: " + response);
-		try {
-                    Thread.sleep((int)(Math.random() * 1000));
-                } catch (InterruptedException e) {
-                	}
+        				long beforeSendingTime = System.nanoTime();
+        				output.writeBytes(message);
+        				response = server.readLine();
+        				long afterReceivingTime = System.nanoTime();
+        				long rttForThisRequest = afterReceivingTime - beforeSendingTime;
+        				               
+        				totalTimeSpentPerClient += rttForThisRequest;
+                
+        				System.out.println("[" + new Date() + "] Received: " + response);
+        				
+        				if (i == NUMBER_OF_REQUESTS_PER_CLIENTS)
+        				{
+        					averageTimeSpentPerClient = totalTimeSpentPerClient/NUMBER_OF_REQUESTS_PER_CLIENTS;
+        					System.out.println("[Average time spent for client " + userid+ "] " + averageTimeSpentPerClient);
+        				}
+        				
+        				
+        				try {
+        					Thread.sleep((int)(Math.random() * 1000));
+        				} catch (InterruptedException e) {
+        				}
 		
-	socket.close();
+        				socket.close();
  	
-	} catch (IOException e) {
-                e.printStackTrace();
-        }
-	}
+        				} catch (IOException e) {
+        					e.printStackTrace();
+        				}
+        		}
+        	
+        	
         }
 
 
@@ -66,16 +83,14 @@ public class TCPClient implements Runnable {
 	public static void main(String args[]) throws IOException {
     	IP = args[0];
     	Port = args[1];
-    	
-        int cntr = 0;
-		while (cntr < NUMBER_OF_CLIENTS) {
-		    TCPClient tcpClient = new TCPClient(cntr);
+    	long tmpTotalAverageTimeSpentPerClient = 0;
+
+        int userid = 1;
+		while (userid <= NUMBER_OF_CLIENTS) {
+		    TCPClient tcpClient = new TCPClient(userid);
 		    TCP_WORKER_SERVICE.submit(tcpClient);
-		    
-		    cntr ++;
+		    userid ++;
 		}
-
-
     }
 
 }
